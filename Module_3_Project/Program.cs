d533102a6c46b5bc8001d4dd0b31c819c6151e1c
Name <erofeev001@gmail.com>
2022-04-29 22:52:44 +0300

    3.3.2

--- Module_3_Project/Program.cs
+++ Module_3_Project/Program.cs
@@ -5,11 +5,15 @@ class MainClass
 	public static void Main(string[] args)
 	{
 
-		string myName;
-		myName = "Игорь";
+		string MyName = "Igor";
+		byte MyAge = 28;
+		bool HaveIApet = true;
+		double MyShoeSize = 45;
 
-		Console.WriteLine(myName);
+		Console.WriteLine("My name is " + MyName);
+		Console.WriteLine("MyAge " + MyAge);
+		Console.WriteLine("Do I have a pet? " + HaveIApet);
+		Console.WriteLine("My shoe size is " + MyShoeSize);
 
-		Console.ReadKey();
 	}
 }
