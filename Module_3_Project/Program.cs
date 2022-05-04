2171d1f671dd14cc306b96967a241c5235050f3f
Name <erofeev001@gmail.com>
2022-05-04 20:59:13 +0300

    Module 3 final task

--- Module_3_Project/Program.cs
+++ Module_3_Project/Program.cs
@@ -4,16 +4,17 @@ class MainClass
 {
 	public static void Main(string[] args)
 	{
+		Console.Write("Enter your name? ");
+		var name = Console.ReadLine();
+		Console.Write("Enter your age? ");
+		var age = checked((byte)int.Parse(Console.ReadLine()));
+		Console.WriteLine("Your name is {0} and age is {1} ", name, age);
 
-		string MyName = "Igor";
-		byte MyAge = 28;
-		bool HaveIApet = true;
-		double MyShoeSize = 45;
+		Console.Write("Enter your birthdate ");
 
-		Console.WriteLine("My name is " + MyName);
-		Console.WriteLine("MyAge " + MyAge);
-		Console.WriteLine("Do I have a pet? " + HaveIApet);
-		Console.WriteLine("My shoe size is " + MyShoeSize);
+		var birthdate = Console.ReadLine();
+		Console.WriteLine("Your birthdate is {0}", birthdate);
+		Console.ReadKey();	
 
 	}
 }
